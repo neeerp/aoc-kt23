7a81e666107c6041392f24bb7b61d1ffdba4ba1e
David Goldstein <goldstein.g.david@gmail.com>
2023-12-02 12:13:56 -0500

    move helpers into Day01 main scope

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,6 +8,42 @@
  * We want the sum of all the calibration values.
  */
 fun main() {
+    fun find_digits(line: String): Int {
+        var first = -1
+        var last = -1
+        for (c in line) {
+            if (first < 0) {
+                first = c.digitToIntOrNull() ?: first
+            }
+            last = c.digitToIntOrNull() ?: last
+        }
+
+        return first * 10 + last
+    }
+
+    val VALID_STRINGS = mapOf(
+        "1" to 1, "one" to 1,
+        "2" to 2, "two" to 2,
+        "3" to 3, "three" to 3,
+        "4" to 4, "four" to 4,
+        "5" to 5, "five" to 5,
+        "6" to 6, "six" to 6,
+        "7" to 7, "seven" to 7,
+        "8" to 8, "eight" to 8,
+        "9" to 9, "nine" to 9,
+        "0" to 0, "zero" to 0
+    )
+
+    fun find_substrings(line: String): Int {
+        val first = VALID_STRINGS[line.findAnyOf(VALID_STRINGS.keys)?.second]
+        val last = VALID_STRINGS[(line.findLastAnyOf(VALID_STRINGS.keys)?.second)]
+
+        if (first == null || last == null) {
+            throw IllegalArgumentException()
+        }
+
+        return first * 10 + last
+    }
 
     fun part1(input: List<String>): Int {
         return input.map { find_digits(it) }.sum()
@@ -28,41 +64,4 @@ fun main() {
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
-}
-
-fun find_digits(line: String): Int {
-    var first = -1
-    var last = -1
-    for (c in line) {
-        if (first < 0) {
-            first = c.digitToIntOrNull() ?: first
-        }
-        last = c.digitToIntOrNull() ?: last
-    }
-
-    return first * 10 + last
-}
-
-val VALID_STRINGS = mapOf(
-    "1" to 1, "one" to 1,
-    "2" to 2, "two" to 2,
-    "3" to 3, "three" to 3,
-    "4" to 4, "four" to 4,
-    "5" to 5, "five" to 5,
-    "6" to 6, "six" to 6,
-    "7" to 7, "seven" to 7,
-    "8" to 8, "eight" to 8,
-    "9" to 9, "nine" to 9,
-    "0" to 0, "zero" to 0
-)
-
-fun find_substrings(line: String): Int {
-    val first = VALID_STRINGS[line.findAnyOf(VALID_STRINGS.keys)?.second]
-    val last = VALID_STRINGS[(line.findLastAnyOf(VALID_STRINGS.keys)?.second)]
-
-    if (first == null || last == null) {
-        throw IllegalArgumentException()
-    }
-
-    return first * 10 + last
 }
