1dea5d628b011a745c45f37cfbc3a513abb17dc2
David Goldstein <goldstein.g.david@gmail.com>
2023-12-02 12:00:04 -0500

    solve day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,12 +15,15 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { find_substrings(it) }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 142)
+    val testInputPart1 = readInput("Day01_part1_test")
+    check(part1(testInputPart1) == 142)
+
+    val testInputPart2 = readInput("Day01_part2_test")
+    check(part2(testInputPart2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
@@ -35,6 +38,30 @@ fun find_digits(line: String): Int {
             first = c.digitToIntOrNull() ?: first
         }
         last = c.digitToIntOrNull() ?: last
+    }
+
+    return first * 10 + last
+}
+
+val VALID_STRINGS = mapOf(
+    "1" to 1, "one" to 1,
+    "2" to 2, "two" to 2,
+    "3" to 3, "three" to 3,
+    "4" to 4, "four" to 4,
+    "5" to 5, "five" to 5,
+    "6" to 6, "six" to 6,
+    "7" to 7, "seven" to 7,
+    "8" to 8, "eight" to 8,
+    "9" to 9, "nine" to 9,
+    "0" to 0, "zero" to 0
+)
+
+fun find_substrings(line: String): Int {
+    val first = VALID_STRINGS[line.findAnyOf(VALID_STRINGS.keys)?.second]
+    val last = VALID_STRINGS[(line.findLastAnyOf(VALID_STRINGS.keys)?.second)]
+
+    if (first == null || last == null) {
+        throw IllegalArgumentException()
     }
 
     return first * 10 + last
