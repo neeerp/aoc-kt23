c2e146113236f8802c6bdbc532baa5f912dd86cb
David Goldstein <goldstein.g.david@gmail.com>
2023-12-02 11:44:54 -0500

    solve day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,18 @@
+/**
+ * The calibration document consists of lines of text.
+ * Each line originally contained a calibration value that needs to be recovered.
+ *
+ * On each line, the calibration value may be found by combining the first and last digit, in that order, forming a two
+ * digit number.
+ *
+ * We want the sum of all the calibration values.
+ */
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { find_digits(it) }.sum()
     }
+
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,9 +20,22 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun find_digits(line: String): Int {
+    var first = -1
+    var last = -1
+    for (c in line) {
+        if (first < 0) {
+            first = c.digitToIntOrNull() ?: first
+        }
+        last = c.digitToIntOrNull() ?: last
+    }
+
+    return first * 10 + last
 }
