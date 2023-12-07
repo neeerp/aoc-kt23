52d4b2b38ce3c243f5da6d45fbde68df109c967b
David Goldstein <goldstein.g.david@gmail.com>
2023-12-07 09:13:16 -0500

    solve day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,3 +1,4 @@
+import java.util.ArrayList
 import kotlin.math.pow
 
 fun main() {
@@ -22,15 +23,35 @@ fun main() {
 
             if (intersect.isNotEmpty()) 2.0.pow(intersect.size - 1).toInt() else 0
         }
-        println(result)
         return result
+    }
+
+    fun numberOfMatches(card: String): Int {
+        val pair = card.substringAfter(':').split('|')
+
+        val winningNumbers = pair[0].trim().split(' ').filter { it.toIntOrNull() != null }.toSet()
+        val myNumbers = pair[1]!!.trim().split(' ').filter { it.toIntOrNull() != null }.toSet()
+
+        val intersect = winningNumbers.intersect(myNumbers)
+        return intersect.size
     }
 
 
     /**
+     * Scratch cards let you win scratch cards equal to the number of winning numbers you have.
+     * You win that many cards below you.
+     * If card 10 has 5 matching numbers, you win one copy of cards 11, 12, 13, 14, and 15.
      */
     fun part2(input: List<String>): Int {
-        return 0
+        var cards = Array(input.size) { _ -> 1 }
+        for ((num, card) in input.withIndex()) {
+            val matches = numberOfMatches(card)
+            for (i in 1..matches) {
+                cards[num + i] += cards[num]
+            }
+        }
+
+        return cards.sum()
     }
 
     val input = readInput("Day04")
@@ -39,6 +60,6 @@ fun main() {
     check(part1(testInput) == 13)
     part1(input).println()
 
-//    check(part2(testInput) == 467835)
-//    part2(input).println()
+    check(part2(testInput) == 30)
+    part2(input).println()
 }
