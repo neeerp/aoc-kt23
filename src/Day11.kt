2c524efa8506e4cb8aad584aac079c740a258ce5
David Goldstein <goldstein.g.david@gmail.com>
2023-12-16 11:09:20 -0500

    solve day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -2,71 +2,14 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun findGalaxies(input: List<List<Char>>): Set<Pair<Int, Int>> {
-        val galaxies: MutableSet<Pair<Int, Int>> = mutableSetOf()
-        for ((i, line) in input.withIndex()) {
-            for ((j, c) in line.withIndex()) {
-                if (c == '#') {
-                    galaxies.add(Pair(i, j))
-                }
-            }
-        }
-
-        return galaxies
-    }
-
-    fun findGalaxies(input: List<String>): Set<Pair<Int, Int>> {
-        return findGalaxies(input.map { it.toList() })
-    }
-
-    fun transformMap(input: List<String>): List<List<Char>> {
-        val result: MutableList<List<Char>> = mutableListOf()
-        val m = input.size
-        val n = input[0].length
-
-        val galaxies = findGalaxies(input)
-
-        val hDeadSpace = (0..<m).filter { it !in galaxies.map { g -> g.first } }
-        val vDeadSpace = (0..<n).filter { it !in galaxies.map { g -> g.second } }
-
-        for ((i, line) in input.withIndex()) {
-            if (i in hDeadSpace) {
-                result.add(MutableList(n + vDeadSpace.size) { _ -> '.' })
-                result.add(MutableList(n + vDeadSpace.size) { _ -> '.' })
-                continue
-            }
-
-            val cur = mutableListOf<Char>()
-            for ((j, c) in line.withIndex()) {
-                cur.add(c)
-
-                if (j in vDeadSpace) {
-                    cur.add(c)
-                }
-            }
-            result.add(cur)
-        }
-
-        return result
-    }
-
-    fun manhattan(p1: Pair<Int, Int>, p2: Pair<Int, Int>): Int {
+    fun manhattan(p1: Pair<Long, Long>, p2: Pair<Long, Long>): Long {
         return abs(p1.first - p2.first) + abs(p1.second - p2.second)
     }
 
-    /**
-     * Given a map of galaxies, assume that every row/col with no galaxy is duplicated.
-     *
-     * Find the shortest path between each pair of galaxies.
-     */
-    fun part1(input: List<String>): Int {
-        // First, let's parse and transform the map.
-        val universe = transformMap(input)
-        val galaxies = findGalaxies(universe).toList()
-
-        var d = 0
-        for ((i, g1) in galaxies.withIndex()) {
-            for (g2 in galaxies.subList(i, galaxies.size)) {
+    fun findAllPairDistances(pairs: List<Pair<Long, Long>>): Long {
+        var d = 0L
+        for ((i, g1) in pairs.withIndex()) {
+            for (g2 in pairs.subList(i, pairs.size)) {
                 d += manhattan(g1, g2)
             }
         }
@@ -74,17 +17,87 @@ fun main() {
         return d
     }
 
+    fun findGalaxies(input: List<List<Char>>): MutableList<Pair<Long, Long>> {
+        val galaxies: MutableList<Pair<Long, Long>> = mutableListOf()
+        for ((i, line) in input.withIndex()) {
+            for ((j, c) in line.withIndex()) {
+                if (c == '#') {
+                    galaxies.add(Pair(i.toLong(), j.toLong()))
+                }
+            }
+        }
 
-    fun part2(input: List<String>): Int {
-        return 0
+        return galaxies
     }
 
-    val input = readInput("Day11")
-    val testInputPart1 = readInput("Day11_test")
+    fun findGalaxies(input: List<String>): MutableList<Pair<Long, Long>> {
+        return findGalaxies(input.map { it.toList() })
+    }
 
-    check(part1(testInputPart1) == 374)
+    fun findSparseGalaxies(input: List<String>, age: Long): List<Pair<Long, Long>> {
+        val m = input.size
+        val n = input[0].length
+
+        var galaxies = findGalaxies(input).sortedByDescending { it.first }.reversed().toMutableList()
+
+        val hDeadSpace = (0L..<m).filter { it !in galaxies.map { g -> g.first } }
+        val vDeadSpace = (0L..<n).filter { it !in galaxies.map { g -> g.second } }
+
+
+        // Incorporate row dead space
+        var deadSpace = 0
+        for ((idx, g) in galaxies.withIndex()) {
+            while (deadSpace < hDeadSpace.size && hDeadSpace[deadSpace] < g.first) {
+                deadSpace += 1
+            }
+
+            galaxies[idx] = Pair(g.first + ((age - 1) * deadSpace), g.second)
+        }
+
+
+        // Incorporate row dead space
+        galaxies = galaxies.sortedByDescending { it.second }.reversed().toMutableList()
+        deadSpace = 0
+        for ((idx, g) in galaxies.withIndex()) {
+            while (deadSpace < vDeadSpace.size && vDeadSpace[deadSpace] < g.second) {
+                deadSpace += 1
+            }
+
+            galaxies[idx] = Pair(g.first, g.second + ((age - 1) * deadSpace))
+        }
+
+        return galaxies
+    }
+
+
+    /**
+     * Same deal... only this time, dead space accounts for 1_000_000 rows/cols instead of 2.
+     */
+    fun part2(input: List<String>, age: Long): Long {
+        val galaxies = findSparseGalaxies(input, age)
+        val d = findAllPairDistances(galaxies)
+        return d
+    }
+
+    /**
+     * Given a map of galaxies, assume that every row/col with no galaxy is duplicated.
+     *
+     * Find the shortest path between each pair of galaxies.
+     */
+    fun part1(input: List<String>): Long {
+        return part2(input, 2)
+    }
+
+
+    val input = readInput("Day11")
+    val testInput = readInput("Day11_test")
+
+    check(part1(testInput) == 374L)
     part1(input).println()
 
-//    check(part2(testInputPart1) == 2286)
-//    part2(input).println()
+    check(part2(testInput, 2L) == 374L)
+    check(part2(testInput, 10L) == 1030L)
+    check(part2(testInput, 100L) == 8410L)
+    check(part2(input, 2) == 9403026L)
+    part2(input, 1000000L).println()
 }
