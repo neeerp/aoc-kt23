27862aa351d68af8d2fa5677e2dab3c10071b9b8
David Goldstein <goldstein.g.david@gmail.com>
2023-12-18 10:09:33 -0500

    solve day 18 part 2

--- src/Day18.kt
+++ src/Day18.kt
@@ -2,36 +2,31 @@ import kotlin.math.abs
 
 fun main() {
 
-    fun shoelace(points: List<Pair<Int,Int>>): Int {
+    fun shoelace(points: List<Pair<Long,Long>>): Long {
         val n = points.size
-        var area = 0
+        var area = 0L
         for (i in 0..<n) {
             val (xi, yi) = points[i]
             val (xip1, yip1) = points[(i+1) % n]
             area += (xip1 + xi) * (yip1 - yi)
         }
-        area = abs(area) / 2
+        area = abs(area) / 2L
 
         return area
     }
 
-    fun part1(input: List<String>): Int {
-        val moves = input.map {
-            var (dir, m, _) = it.split(' ')
-            Pair(dir[0], m.toInt())
-        }
-
-        var pos = Pair(0, 0)
-        var perimeter = 0
+    fun computeNumberOfPoints(moves: List<Pair<Char,Long>>): Long {
+        var pos = Pair(0L, 0L)
+        var perimeter = 0L
         val points = mutableListOf(pos)
 
         for ((d, m) in moves) {
             val (i, j) = pos
             pos = when (d) {
-                'R' -> Pair(i, j + m)
-                'L' -> Pair(i, j - m)
-                'D' -> Pair(i + m, j)
-                'U' -> Pair(i - m, j)
+                'R', '0' -> Pair(i, j + m)
+                'L', '2' -> Pair(i, j - m)
+                'D', '1' -> Pair(i + m, j)
+                'U', '3' -> Pair(i - m, j)
                 else -> throw Exception()
             }
 
@@ -43,21 +38,39 @@ fun main() {
 
         // Using Pick's theorem...
         // A = interior + boundary/2 - 1
-        val interior = area - perimeter/2 + 1
+        val interior = area - perimeter/2L + 1L
 
         return interior + perimeter
     }
 
-    fun part2(input: List<List<Int>>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        val moves = input.map {
+            var (dir, m, _) = it.split(' ')
+            Pair(dir[0], m.toLong())
+        }
+
+        return computeNumberOfPoints(moves)
+    }
+
+
+    fun part2(input: List<String>): Long {
+        val moves = input.map {
+            var (_, temphex) = it.split('#')
+            val hex = temphex.dropLast(1)
+            val d = hex.last()
+            val m = hex.dropLast(1).toLong(radix = 16)
+            Pair(d, m)
+        }
+
+        return computeNumberOfPoints(moves)
     }
 
     val input = readInput("Day18")
     val testInput = readInput("Day18_test")
 
-    check(part1(testInput) == 62)
+    check(part1(testInput) == 62L)
     part1(input).println()
 
-//    check(part2(testInput) == 94)
-//    part2(input).println()
+    check(part2(testInput) == 952408144115L)
+    part2(input).println()
 }
